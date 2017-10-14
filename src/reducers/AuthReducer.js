cd424420cdb2aa8ffea48a19e9530d397e3ef761
akodate <alex.kodate@gmail.com>
2017-10-14 18:54:43 +0700

    Add LOGIN_USER_SUCCESS and LOGIN_USER_FAIL actions

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,11 +1,15 @@
 import { 
   EMAIL_CHANGED, 
   PASSWORD_CHANGED,
+  LOGIN_USER_FAIL,
+  LOGIN_USER_SUCCESS,
 } from '../actions/types';
 
 const INITIAL_STATE = { 
   email: '',
+  error: '',
   password: '',
+  user: null,
 };
 
 export default (state=INITIAL_STATE, action) => {
@@ -14,6 +18,10 @@ export default (state=INITIAL_STATE, action) => {
   switch (action.type) {
     case EMAIL_CHANGED:
       return { ...state, email: action.payload };
+    case LOGIN_USER_FAIL:
+      return { ...state, error: 'Authentication failed.', password: '' };
+    case LOGIN_USER_SUCCESS:
+      return { ...state, error: '', user: action.payload };
     case PASSWORD_CHANGED:
       return { ...state, password: action.payload };
     default:
